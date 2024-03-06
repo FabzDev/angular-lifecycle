18e1442592235c58620bc3d5a791248fbc09b2aa
Fabio <fabioescobarardila@hotmail.com>
2024-03-05 20:20:17 -0500

    SidebarComponent completado

--- src/app/maps/components/sidebar/sidebar.component.ts
+++ src/app/maps/components/sidebar/sidebar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface MenuItems {
+  route: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: ``
+  styles:`li{ cursor: pointer; transition: 0.2s all }`
 })
 export class SidebarComponent {
+
+  public menuItems: MenuItems[] = [
+    { route: 'fullscreen', name: 'FullScreen' },
+    { route: 'zoom-range', name: 'ZoomRange' },
+    { route: 'marks', name: 'Marks' },
+    { route: 'properties', name: 'Houses' },
+  ]
 
 }
--- src/app/maps/layouts/maps-layout/maps-layout.component.ts
+++ src/app/maps/layouts/maps-layout/maps-layout.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'maps-layout',
   templateUrl: './maps-layout.component.html',
-  styles: ``
+  styleUrl: './maps-layout.component.css'
 })
 export class MapsLayoutComponent {
 
