9477da230b5ac8da7870e946bb400fffb4505c8f
Fabio <fabioescobarardila@hotmail.com>
2024-03-05 23:32:48 -0500

    MapLibre implementado y funcionando al 100%

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Map } from 'maplibre-gl';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styles: ``
+  styleUrl: 'full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements OnInit, AfterViewInit, OnDestroy{
+
+  map?: Map;
+
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+
+  constructor(){};
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngAfterViewInit(): void {
+    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=vDOrj6jFitb3t7hjxI2N`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    });
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
 
 }
