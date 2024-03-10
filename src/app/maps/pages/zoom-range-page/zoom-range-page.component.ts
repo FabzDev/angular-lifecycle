e1a831be52ccff735c573389a41d0d29c3481f23
Fabio <fabioescobarardila@hotmail.com>
2024-03-10 15:15:08 -0500

    Implementando ZoomIn y ZoomOut

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'maplibre-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
-  styles: ``
+  styleUrl: 'zoom-range-page.css'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit{
+
+  map?: Map;
+
+  @ViewChild('mapa')
+  private mapContainer!: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    const initialState = { lng: -75.46171, lat: 10.39161, zoom: 14 };
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=vDOrj6jFitb3t7hjxI2N`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    });
+  }
 
 }
