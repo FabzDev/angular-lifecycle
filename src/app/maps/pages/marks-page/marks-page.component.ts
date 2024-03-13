acfb7ec31da9dc5b9526ca7caccc1012a7e8b56e
Fabio <fabioescobarardila@hotmail.com>
2024-03-12 20:59:39 -0500

    Implementando marks component completo

--- src/app/maps/pages/marks-page/marks-page.component.ts
+++ src/app/maps/pages/marks-page/marks-page.component.ts
@@ -1,10 +1,17 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { LngLat, Map, Marker } from 'maplibre-gl'
+import { Map, Marker } from 'maplibre-gl';
+
 
 interface MarkerAndColor {
-  color: string,
-  marker: Marker
+  marker: Marker;
+  color: string;
 }
+
+interface CoordinateAndColor {
+  lnglat: [number, number];
+  color: string;
+}
+
 
 @Component({
   selector: 'maps-page-marks',
@@ -13,61 +20,94 @@ interface MarkerAndColor {
 })
 export class MarksPageComponent {
   public map?: Map;
-  public markers: MarkerAndColor[] = [];
-
+  public markersAndColors: MarkerAndColor[] = [];
+  public coordinatesAndColors: CoordinateAndColor[] = [];
   @ViewChild('map')
   private mapContainer!: ElementRef<HTMLElement>;
 
-  constructor(){};
+  constructor() {}
 
   ngOnInit(): void {
+    this.readCoordsFromLS();
   }
 
   ngAfterViewInit(): void {
     const initialState = { lng: -75.50021, lat: 10.40072, zoom: 15 };
-
     this.map = new Map({
       container: this.mapContainer.nativeElement,
       style: `https://api.maptiler.com/maps/streets-v2/style.json?key=vDOrj6jFitb3t7hjxI2N`,
       center: [initialState.lng, initialState.lat],
-      zoom: initialState.zoom
+      zoom: initialState.zoom,
     });
 
-    const marker = new Marker({
-      color: 'red'
+    new Marker({
+      color: 'red',
     })
-    .setLngLat(this.map.getCenter())
-    .addTo(this.map)
+      .setLngLat(this.map.getCenter())
+      .addTo(this.map);
 
-    }
+    this.buildMarkers();
 
-    addMarker(): void {
-      // const markerElementHtml = document.createElement('div');
-      // markerElementHtml.innerHTML = 'Apto <br> disponible';
-      const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16))
-      const marker = new Marker({
-        // element: markerElementHtml,
-        color: color,
-        draggable: true,
-      })
-      .setLngLat(this.map!.getCenter())
-      .addTo(this.map!)
-
-      this.markers.push({ color: color, marker: marker });
-
-    }
-
-    deleteMarker(index: number){
-      this.markers[index].marker.remove();
-      this.markers.splice(index, 1);
-    }
-
-    moveToMarker(mark: Marker){
-      if (!this.map) return;
-      this.map.flyTo({
-        zoom: 18,
-        center: mark.getLngLat(),
-
-      })
-    }
   }
+
+  addMarker(): void {
+    if (!this.map) throw Error('Mapa no encontrado');
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const currentCoordinates = this.map.getCenter().toArray();
+    this.createNewMarker({ lnglat: currentCoordinates, color: color });
+    this.writeLocalStorage();
+  }
+
+  createNewMarker(coordAndColor: CoordinateAndColor): void {
+    const newMarker = new Marker({
+      color: coordAndColor.color,
+      draggable: true,
+    })
+    .setLngLat(coordAndColor.lnglat)
+    .addTo(this.map!);
+    newMarker.on('dragend', () => { this.writeLocalStorage(); console.log('action');
+     })
+    this.markersAndColors.push({marker: newMarker, color: coordAndColor.color})
+  }
+
+  writeLocalStorage() {
+    this.coordinatesAndColors = [];
+    this.markersAndColors.forEach(({ marker, color }) => {
+      this.coordinatesAndColors.push({
+        lnglat: marker.getLngLat().toArray(),
+        color: color,
+      });
+    });
+    localStorage.setItem(
+      'coordinatesAndColors',
+      JSON.stringify(this.coordinatesAndColors)
+    );
+    console.log( localStorage.getItem('coordinatesAndColors') );
+  }
+
+  deleteMarker(index: number) {
+    this.markersAndColors[index].marker.remove();
+    this.markersAndColors.splice(index, 1);
+    this.writeLocalStorage();
+  }
+
+  readCoordsFromLS() {
+    this.coordinatesAndColors = JSON.parse(
+      localStorage.getItem('coordinatesAndColors') ?? '[]'
+    );
+  }
+
+  buildMarkers(): void {
+    this.coordinatesAndColors.forEach(({ lnglat, color }) => this.createNewMarker({ lnglat, color }));
+  }
+
+  moveToMarker(mark: Marker) {
+    if (!this.map) return;
+    this.map.flyTo({
+      zoom: 18,
+      center: mark.getLngLat(),
+    });
+  }
+}
